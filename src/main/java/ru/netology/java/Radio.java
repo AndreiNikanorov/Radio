9ba6b269551702c93a1643f015423c98dd688d5b
Andrei Nikanorov <flea07one@yandex.ru>
2022-08-13 14:36:02 +0300

    Sound level increased and station selector added

--- src/main/java/ru/netology/java/Radio.java
+++ src/main/java/ru/netology/java/Radio.java
@@ -1,63 +1,72 @@
 package ru.netology.java;
 
-
 public class Radio {
     private int currentVolume;
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0) {
-            currentVolume = 0;
-        } else if (newCurrentVolume > 10) {
-            currentVolume = 10;
-        } else
-        currentVolume = newCurrentVolume;
-    }
-
-    public void decreaseVolume() {
-        if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
-        }
-    }
-
-    public void increaseVolume() {
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
-        }
-    }
-
     private int currentStation;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+    private int allStations = 10;
+    private int minStation = 0;
+
+    public Radio(int allStations) {
+        this.allStations = allStations;
+    }
+
+    public Radio() {
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+        if (newCurrentStation < minStation) {
             return;
         }
-        if (newCurrentStation > 9) {
+        if (newCurrentStation > allStations - 1) {
             return;
         }
         currentStation = newCurrentStation;
     }
 
     public void next() {
-        if (currentStation < 9) {
+        if (currentStation < allStations - 1) {
             currentStation = currentStation + 1;
         } else {
-            currentStation = 0;
+            currentStation = minStation;
         }
     }
 
     public void prev() {
-        if (currentStation > 0) {
+        if (currentStation > minStation) {
             currentStation = currentStation - 1;
         } else {
-            currentStation = 9;
+            currentStation = allStations - 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < minVolume) {
+            currentVolume = minVolume;
+        } else if (newCurrentVolume > maxVolume) {
+            currentVolume = maxVolume;
+        } else
+        currentVolume = newCurrentVolume;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
         }
     }
 }
